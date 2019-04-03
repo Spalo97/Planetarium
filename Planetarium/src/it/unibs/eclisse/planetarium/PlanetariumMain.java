e90e9ca35c49cb49d09c74db90a904f9aef24f0c
Lange99 <c.langellotti@studenti.unibs.it>
2019-04-03 15:02:30 +0200

    inserimento funzione setPlanet

--- Planetarium/src/it/unibs/eclisse/planetarium/PlanetariumMain.java
+++ Planetarium/src/it/unibs/eclisse/planetarium/PlanetariumMain.java
@@ -1,24 +1,38 @@
 package it.unibs.eclisse.planetarium;
-
 import java.util.*;
 
 public class PlanetariumMain {
-
+	
+	//idPlanet serve per contare quanti pianeti aggiungo correttamente
+	// in modo da gestire l'assegnazione dell'id
+	private int idPlanet = 0;
+	
+	public int getIdPlanet() {
+		return idPlanet;
+	}
+	public void setIdPlanet(int idPlanet) {
+		this.idPlanet = idPlanet;
+	}
+	
 	public static void main(String[] args) {
+		PlanetariumMain idPlanet = new PlanetariumMain();
+		int id=idPlanet.getIdPlanet();
 		
-		int id = 0;
-		
+		//Creazione stella nel sistema solare (s)
 		Planet p = new Planet("sole", id, 100000, 15, 20);
-		
 		PlanetarySystem s = new PlanetarySystem(p);		
-				
+		
+		p= s.setPlanet();
 		if(s.addPianeta(p)) {
 			//print "pianeta aggiunto con successo"
 			id++;
+			idPlanet.setIdPlanet(id);
+			
 		}else {
 			//print "errore nell'inserimento"
 			//test nick
 		}
+		
 		
 	}
 
--- Planetarium/src/it/unibs/eclisse/planetarium/PlanetarySystem.java
+++ Planetarium/src/it/unibs/eclisse/planetarium/PlanetarySystem.java
@@ -9,7 +9,6 @@ public class PlanetarySystem {
 	private boolean endWell = false;
 	
 	public PlanetarySystem(Planet stella) {
-		
 		this.stella = stella;
 	}
 	
@@ -18,4 +17,48 @@ public class PlanetarySystem {
 		endWell=pianeti.add(p);
 		return endWell;
 	}
+
+
+	public static Planet setPlanet() {
+		Scanner lettore = new Scanner(System.in);
+		PlanetariumMain idP = new PlanetariumMain();
+		
+		//input da tastiera per il nome del pianeta
+		String name=null;
+		do {
+			System.out.println("inserisci nome pianeta: ");
+			name=lettore.nextLine();
+		}while(name==null || name.isEmpty());
+		
+		// input massa
+		double mass;
+		do {
+			System.out.println("inserisci massa: ");
+			mass=lettore.nextDouble();
+		}while(mass==0);
+		
+		//incremento id
+		int id=idP.getIdPlanet();
+		id++;
+		
+		//input x 
+		int x=0;
+		do {
+			System.out.println("inserisci coordinata x: ");
+			x=lettore.nextInt();
+		}while(x==0);
+		
+		//input y
+		int y=0;
+		do {
+			System.out.println("inserisci coordinata x: ");
+			x=lettore.nextInt();
+		}while(y==0);
+		
+		Planet p = new Planet(name, mass, id, x, y);
+		
+		return p;
+	}
+	
+	
 }
